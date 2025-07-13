24eb698dad31ded821db35bba0f1cc520b7fd14d
MahaGit1233 <maharush5409@gmail.com>
2025-07-13 20:20:13 +0530

    login page frontend commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import Signup from "./components/signup";
+import Login from "./components/login";
 
 function App() {
+  const [isLogin, serIsLogin] = useState(false);
+
+  const toggleAuth = () => {
+    serIsLogin((prev) => !prev);
+  };
+
   return (
-    <div>
-      <Signup />
+    <div style={{ paddingTop: "7%" }}>
+      <p
+        style={{
+          textAlign: "center",
+          cursor: "pointer",
+          color: "blue",
+          textDecoration: "underline",
+        }}
+        onClick={toggleAuth}
+      >
+        {isLogin
+          ? "Don't have an account? Sign up now!"
+          : "Already signed up? Login now!"}
+      </p>
+      {isLogin ? <Login /> : <Signup />}
     </div>
   );
 }
---
+++ src/components/login.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { Alert, Button, Card, Form } from "react-bootstrap";
+import "./signup.css";
+
+const Login = () => {
+  const [enteredMail, setEnteredMail] = useState("");
+  const [enteredPass, setEnteredPass] = useState("");
+  const [error, setError] = useState("");
+
+  const mailChangeHandler = (event) => {
+    setEnteredMail(event.target.value);
+  };
+
+  const passChangeHandler = (event) => {
+    setEnteredPass(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredMail || !enteredPass) {
+      setError("All the fields are required to be filled");
+      return;
+    }
+
+    fetch("http://localhost:4000/users/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredMail,
+        password: enteredPass,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          console.log("User has successfully logged in");
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            throw new Error(data.message || "Login failed");
+          });
+        }
+      })
+      .catch((err) => {
+        alert(err.message);
+        console.log(err.message);
+      });
+
+    setEnteredMail("");
+    setEnteredPass("");
+    setError("");
+  };
+
+  return (
+    <div className="signup">
+      <Card className="card">
+        <Form className="form" onSubmit={formSubmitHandler}>
+          <h1 style={{ textAlign: "center", paddingTop: "0.6rem" }}>Login</h1>
+          <Form.Group>
+            <Form.Label className="formlabel">Email ID:</Form.Label>
+            <Form.Control
+              className="forminput"
+              type="email"
+              placeholder="Enter your mail id"
+              value={enteredMail}
+              onChange={mailChangeHandler}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label className="formlabel">Password:</Form.Label>
+            <Form.Control
+              className="forminput"
+              type="password"
+              placeholder="Enter your password"
+              value={enteredPass}
+              onChange={passChangeHandler}
+            />
+          </Form.Group>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <div
+            style={{
+              textAlign: "center",
+              paddingBottom: "0.7rem",
+              paddingTop: "0.3rem",
+            }}
+          >
+            <Button type="submit" variant="outline-dark">
+              Login
+            </Button>
+          </div>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
