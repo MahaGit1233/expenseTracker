6afe284bb4243f99e7897b42e2a30a11ef512ad3
MahaGit1233 <maharush5409@gmail.com>
2025-07-13 20:28:55 +0530

    updated controller commit

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -34,8 +34,12 @@ const loginUsers = async (req, res) => {
   try {
     const user = await Users.findOne({ where: { email } });
 
-    if (!user || user.password !== password) {
-      return res.status(401).json({ message: "Invalid email or password" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Incorrect password" });
     }
 
     res.status(200).json({ message: "Login successful" });
