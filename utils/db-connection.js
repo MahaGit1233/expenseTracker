5edca511829bdb614c3904b41da0dbe53e99e18d
MahaGit1233 <maharush5409@gmail.com>
2025-08-06 11:20:24 +0530

    utils commit

--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-console.log('db name:',process.env.DB_NAME);
-console.log('db password:',process.env.DB_PASSWORD);
+console.log("db name:", process.env.DB_NAME);
+console.log("db password:", process.env.DB_PASSWORD);
 
-const sequelize = new Sequelize(process.env.DB_NAME, "root", process.env.DB_PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 (async () => {
   try {
